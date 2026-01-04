b4eb3510b36f3e62f78f9b2b3b42e689df15ec65
sagarCS0 <sagargupta.cs04@gmail.com>
2026-01-04 21:25:55 +0545

    Test backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,24 @@
-ÿþ
+from fastapi import FastAPI
+
+app = FastAPI(title="Resource Management System API")
+
+@app.get("/")
+def root():
+    return {
+        "status": "Backend is running",
+        "message": "RMS API working correctly"
+    }
+
+@app.get("/api/health")
+def health_check():
+    return {
+        "backend": "ok",
+        "database": "not checked"
+    }
+
+
+# To test backend is working or not. Do this steps:
+# 
+# 1. cd backend
+# 2. uvicorn app.main:app --reload
+# 3. Open these urls http://127.0.0.1:8000/ or http://127.0.0.1:8000/api/health
