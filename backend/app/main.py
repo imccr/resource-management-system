aa337fcd9707e59d2e3555dc4c710c0a2e779f21
sagarCS0 <sagargupta.cs04@gmail.com>
2026-01-04 21:35:56 +0545

    Check frontend and backend integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Resource Management System API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
